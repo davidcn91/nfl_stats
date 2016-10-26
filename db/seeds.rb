959b412995f372ae633d2c9bef7d869d5fd6f5f6
David Nelson <davidcnelson9491@gmail.com>
2016-10-26 12:38:18 -0400

    seed all teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+team_list = [["Dallas","Cowboys","DAL"],["New York","Giants","NYG"],["Philadelphia","Eagles","PHI"],
+["Washington","Redskins","WAS"],["Chicago","Bears","CHI"],["Detroit","Lions","DET"],
+["Green Bay","Packers","GB"],["Minnesota","Vikings","MIN"],["Atlanta","Falcons","ATL"],
+["Carolina","Panthers","CAR"],["New Orleans","Saints","NO"],["Tampa Bay","Buccaneers","TB"],
+["Arizona","Cardinals","ARZ"],["Los Angeles","Rams","LA"],["San Francisco","49ers","SF"],
+["Seattle","Seahawks","SEA"],["Buffalo","Bills","BUF"],["Miami","Dolphins","MIA"],
+["New England","Patriots","NE"],["New York","Jets","NYJ"],["Baltimore","Ravens","BAL"],
+["Cincinnati","Bengals","CIN"],["Cleveland","Browns","CLE"],["Pittsburgh","Steelers","PIT"],
+["Houston","Texans","HOU"],["Indianapolis","Colts","IND"],["Jacksonville","Jaguars","JAX"],
+["Tennessee","Titans","TEN"],["Denver","Broncos","DEN"],["Kansas City","Chiefs","KC"],
+["Oakland","Raiders","OAK"],["San Diego","Chargers","SD"]]
+
+team_list.each do |location, name, abbreviation|
+  Team.create(location: location, name: name, abbreviation: abbreviation)
+end
