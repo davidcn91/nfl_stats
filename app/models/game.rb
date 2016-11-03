0db38da3b4586f9a94e792a1249e30be6c9b8515
David Nelson <davidcnelson9491@gmail.com>
2016-11-03 11:35:05 -0400

    same teams error

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,14 @@
 class Game < ActiveRecord::Base
   validates :season, :week, :away_team_id, :home_team_id, :away_score, :home_score, :spread, presence: true
   validates :away_score, :home_score, numericality: { greater_than_or_equal_to: 0 }
+  validate :teams_cannot_be_same
+
+
+  def teams_cannot_be_same
+    if away_team_id == home_team_id
+      errors.add(:away_team_id, "can't be the same as home team")
+    end
+  end
 
   belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
   belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
