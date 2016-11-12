28001d347e3b8e3f12fb01af3de47228fcb509e7
David Nelson <davidcnelson9491@gmail.com>
2016-11-12 12:27:45 -0500

    team dropdown spec

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,9 +6,10 @@ class TeamsController < ApplicationController
 
   def show
     if params[:id] == "dropdown_selection"
-      params[:id] = Team.where(name: params[:teams][:name])[0]
+      @team = Team.where(name: params[:teams][:name])[0]
+    else
+      @team = Team.find(params[:id])
     end
-    @team = Team.find(params[:id])
     @team_name = @team.name
     away_games = Game.where(away_team_id: @team.id)
     home_games = Game.where(home_team_id: @team.id)
--- spec/features/team/user_sees_team_page_spec.rb
+++ spec/features/team/user_sees_team_page_spec.rb
@@ -7,6 +7,7 @@ feature 'user views team page', %Q{
   # * I should be able to view a team's page by clicking a link from the root index.
   # * I should be able to see the team's name and all games it has played.
   # * I should be able to click a link for each game to view more details of that game.
+  # * I should be able to choose any team from a dropdown on any page in the app.
 
   before(:each) do
     @team_1 = FactoryGirl.create(:team)
@@ -19,6 +20,23 @@ feature 'user views team page', %Q{
 
     expect(page).to have_content(@team_1.location)
     expect(page).to have_content(@team_1.name)
+    expect(page).to have_content("Games")
+  end
+
+  scenario "user selects team from dropdown" do
+    visit root_path
+    select @team_1.name, from: "teams_name"
+    click_button "teams_button"
+
+    expect(page).to have_content(@team_1.location)
+    expect(page).to have_content(@team_1.name)
+    expect(page).to have_content("Games")
+
+    select @team_2.name, from: "teams_name"
+    click_button "teams_button"
+
+    expect(page).to have_content(@team_2.location)
+    expect(page).to have_content(@team_2.name)
     expect(page).to have_content("Games")
   end
 
