f7c85a2781a7bd8bd65487dd20d0d255339d951f
David Nelson <davidcnelson9491@gmail.com>
2016-12-20 10:22:00 -0500

    delete app js content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-$(document).ready(function() {
-  $("#accordion > li > div").click(function(){
-
-  	if(false == $(this).next().is(':visible')) {
-  		$('#accordion ul').slideUp(300);
-  	}
-  	$(this).next().slideToggle(300);
-  });
-
-  $('#accordion ul:eq(0)').show();
-});
