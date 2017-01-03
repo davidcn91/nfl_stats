80d3e345ed1b5744929d261c8332a6391c1e86cc
David Nelson <davidcnelson9491@gmail.com>
2017-01-03 15:37:42 -0500

    remove teamrank module

--- app/helpers/team_ranker.rb
+++
@@ -1,8 +0,0 @@
-module TeamRanker
-
-  def rank(teams)
-    teams
-    # teams.sort_by {|t| [t.win_percentage(@season, spread),t.record(@season, spread)[0],(t.record(@season, spread)[1]*-1)]}.reverse
-  end
-
-end
